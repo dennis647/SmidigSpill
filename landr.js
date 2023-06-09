98b351e5d0aaa8b81ba674217d0c1ac45cf32520
ArsalaanSuleman <105915521+ArsalaanSuleman@users.noreply.github.com>
2023-06-09 12:02:46 +0200

    landr js

---
+++ landr.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("load", function() {
+    document.getElementById("popup").style.display = "flex";
+    });
+
+    document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+    });
